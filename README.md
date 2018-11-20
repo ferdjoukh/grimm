83cfd76e4fa166ebf83e505266210a804391aaf8
Adel Ferdjoukh <ferdjoukh@gmail.com>
2018-11-20 16:45:03 +0100

    update readme for grimm v5.0

--- README.md
+++ README.md
@@ -30,6 +30,24 @@ grimm is the centerpiece of more other works (papers and tools) done by the same
 
 ## Releases
 
+- [v5.0-d20112018](https://github.com/ferdjoukh/grimm/releases/tag/v5.0-d20112018) (November 20th 2018). This version add following features:
+
+	- Adding random diversity when assigning values for attributes of type: EEnum, EInt, EBoolean and EString.
+	- EString attributes have 2 default configuration modes:
+		- **name** is applied for name attributes. Each attribute is named following his class and EObject ID (eg. Method4).
+		- **random** is applied for other attributes. A random string is generated (3 to 10 alphabetic symbols).
+	- Allow the users to give custom domains for EInt and EString by using a [configuration file](#configuration-file) (.grimm)	
+		- For **EInt** attributes, users can give either an interval (25..30) or a list of values (-1 0 1). This is useful for example in UML meta-model to set up the lower and upper bound of an Association.
+		- For **EString** attributes, users can give a list of values. It is interesting to get more customisable models.
+	- Variables that represent attributes in CSP are removed. Instantiation is done as a post-processing (after the CSP solver).
+	- Default interval for EInt attributes us set to 1..100 (can be changes by using a config file).
+	- Corrected bug: Attribute types are printed in config files as a reminder.
+	- Corrected bug: A generated pre-filled config file is now consistent and can be used to generate an empty model.
+	- Corrected bug: Boolean and EEnum attribute are removed from config files.	
+	- Corrected bug: created config files are stored in the folder chosen by the user and not in *rootClass* folder.
+	- Corrected bug: problem of instantiation for 0..* references when reference UB is set to 1.	
+
+
 - [v4.0-d15112018](https://github.com/ferdjoukh/grimm/releases/tag/v4.0-d15112018) (November 15th 2018). This version add following features:
 
 	- Add specific processing for containment references. Now it is different from classic references and not included in the produced CSP instances.
--- src/gui/Help.java
+++ src/gui/Help.java
@@ -9,6 +9,8 @@ public class Help {
 		System.out.println(" is a tool for automated model generation (meta-model instantiation)");
 		
 		System.out.println("");
+		System.out.println("USAGE");
+		System.out.println("");
 		System.out.println("    java -jar grimm.jar [options]");
 		
 		System.out.println("");
@@ -19,41 +21,44 @@ public class Help {
 //		System.out.println("     ");
 //	
 		System.out.println("");
-		System.out.println(" help (shortcut: h)");
-		System.out.println("     print help page");
+		System.out.println("    help (shortcut: h)");
+		System.out.println("       print help page");
 		
 		
 		System.out.println("");
-		System.out.println(" parameter filePath.params (shortcut: p)");
-		System.out.println("     Create a pre-filled parameter file (=filePath.params) for generation");
-		System.out.println("     It contains general information on generation: ");
-		System.out.println("       meta-model, OCL file, root class, output format, number of solutions, etc");
+		System.out.println("    parameter filePath.params (shortcut: p)");
+		System.out.println("        Create a pre-filled parameter file (=filePath.params) for generation");
+		System.out.println("         It contains general information on generation: ");
+		System.out.println("          meta-model, OCL file, root class, output format, number of solutions, etc");
 		
 		
 		System.out.println("");
-		System.out.println(" config filePath.grimm metamodel.ecore rootClass (shortcut: c)");
-		System.out.println("     create a configuration file (=filePath.grimm) for the given meta-model");
-		System.out.println("     It contains specific information on a meta-model:");
-		System.out.println("       number of instances for classes, domains for attributes probability distributions for diversity, etc");
+		System.out.println("    config filePath.grimm metamodel.ecore rootClass (shortcut: c)");
+		System.out.println("        create a configuration file (=filePath.grimm) for the given meta-model");
+		System.out.println("         It contains specific information on a meta-model:");
+		System.out.println("          number of instances for classes, custom domains for attributes, references UpperBound");
 		
 		System.out.println("");
-		System.out.println(" generate filePath.params (shortcut: g)");
-		System.out.println("     Generate models using the given parameters file");
+		System.out.println("    generate filePath.params (shortcut: g)");
+		System.out.println("         Generate models using the given parameters file");
 	
 		
 		System.out.println("");
 		System.out.println("QUICK START");
 		
 		System.out.println("");
-		System.out.println(" a quick start example is included in the version of GRIMM you downloaded");
-		System.out.println("     java -jar grimm.jar g examples/testMM1.params");
+		System.out.println("     a quick start example is included in the version of GRIMM you downloaded");
+		System.out.println("");
+		System.out.println("       java -jar grimm.jar g parameters-files/quick-tests/test1-quick-dot.params");
+		System.out.println("       java -jar grimm.jar g parameters-files/quick-tests/test1-quick-xmi.params");
 		System.out.println("");
 		System.out.println("     The results of execution are stored in Compo folder (named after rootClass)");
 		
 		
 		System.out.println("");
-		System.out.println("EXAMPLES");
-		System.out.println("");
+		System.out.println("CREDITS");
+		System.out.println("    version: v5.0-d20112018 (November 2018)");
+		System.out.println("    author: grimm is totally developped by Adel Ferdjoukh (ferdjoukh@gmail.com)");
 		
 	}
 }
