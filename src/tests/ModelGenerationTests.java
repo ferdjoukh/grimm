2d274005245bb3a5b337d8c779e7f2f3c8d13a0a
Adel Ferdjoukh <ferdjoukh@gmail.com>
2018-11-20 16:00:36 +0100

    Add examples for Ecore mm

--- src/tests/ModelGenerationTests.java
+++ src/tests/ModelGenerationTests.java
@@ -10,20 +10,20 @@ public class ModelGenerationTests {
 	//////////////////////////////
 	@Test
 	public void mapCustomAttributesDot() throws Exception{
-		String [] args= {"g","parameters-files/maps-custom-attributes-dot.params"};
+		String [] args= {"g","parameters-files/maps/maps-custom-attributes-dot.params"};
 		GrimmLauncher.main(args);
 	}
 	
 	@Test
 	public void mapCustomAttributesXmiDot() throws Exception{
-		String [] args= {"g","parameters-files/maps-custom-attributes-xmi.params"};
+		String [] args= {"g","parameters-files/maps/maps-custom-attributes-xmi.params"};
 		GrimmLauncher.main(args);
 	}
 	
 	@Test
 	public void mapNoPublicSpacesXmi() throws Exception{
 		//Testing size 0 for some classes
-		String [] args= {"g","parameters-files/maps-noPublicSpaces.params"};
+		String [] args= {"g","parameters-files/maps/maps-noPublicSpaces.params"};
 		GrimmLauncher.main(args);
 	}
 	
@@ -32,61 +32,35 @@ public class ModelGenerationTests {
 	//////////////////////////////
 	@Test
 	public void scaffoldConfigDot() throws Exception{
-		String [] args= {"g","parameters-files/scaffold-config-dot.params"};
+		String [] args= {"g","parameters-files/ScaffoldGraph/scaffold-config-dot.params"};
 		GrimmLauncher.main(args);
 	}
 	
 	@Test
 	public void scaffoldConfigXmi() throws Exception{
-		String [] args= {"g","parameters-files/scaffold-config-xmi.params"};
+		String [] args= {"g","parameters-files/ScaffoldGraph/scaffold-config-xmi.params"};
 		GrimmLauncher.main(args);
 	}
 	
 	
-	/////////////////////
+	////////////////////////////
 	//  Ecore.ecore
-	////////////////////
+	////////////////////////////
+	@Test
+	public void EcoreEmpty() throws Exception{
+		String [] args= {"g","parameters-files/Ecore/1ecore-emptyEPackage-xmi.params"};
+		GrimmLauncher.main(args);
+	}
+	
+	@Test
+	public void EcoreQuick() throws Exception{
+		String [] args= {"g","parameters-files/Ecore/2ecore-quick-xmi.params"};
+		GrimmLauncher.main(args);
+	}
+	
 	@Test
 	public void Ecore4ATL1() throws Exception{
-		String [] args= {"g","parameters-files/ecore4ATL1.params"};
-		GrimmLauncher.main(args);
-	}
-	
-	//Old tests
-	@Test
-	public void Ecore1() throws Exception{
-		String [] args= {"g","tests/generation/ecore1.params"};
-		GrimmLauncher.main(args);
-	}
-	
-	
-	@Test
-	public void generationEcore4ATLconf1() throws Exception{
-		String [] args= {"g","tests/generation/ecore4ATL-conf.params"};
-		GrimmLauncher.main(args);
-	}
-	
-	@Test
-	public void ecore_conf_use_domains() throws Exception{
-		String [] args= {"g","tests/generation/ecore4ATL-conf-user-domains.params"};
-		GrimmLauncher.main(args);
-	}
-
-	@Test
-	public void generationScaffold() throws Exception{
-		String [] args= {"g","tests/generation/scaffold1.params"};
-		GrimmLauncher.main(args);
-	}
-	
-	@Test
-	public void generateMaps() throws Exception{
-		String [] args= {"g","tests/generation/maps2.params"};
-		GrimmLauncher.main(args);
-	}
-	
-	@Test
-	public void mapsDot() throws Exception{
-		String [] args= {"g","tests/generation/maps-dot.params"};
+		String [] args= {"g","parameters-files/Ecore/3ecore4ATL-xmi1.params"};
 		GrimmLauncher.main(args);
 	}
 }
